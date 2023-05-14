c798b8aac5bad811611bfb58f76963c22a6a2bb2
anushapushpan <anushapushpan2023a@mca.ajce.in>
2023-05-14 19:01:25 +0530

    first commit

--- BeautyParlour/settings.py
+++ BeautyParlour/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-mb4-3dnxz)-0*x5x^3nrh4o5nhzacty3xltpf2sbyj&z9mcpxf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
