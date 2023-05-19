df388ab433bd04c433d175b655812cda01669576
AnushaPushpan15 <anushapushpan2000@gmail.com>
2023-05-19 12:06:07 +0530

    csrf added

--- BeautyParlour/settings.py
+++ BeautyParlour/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-mb4-3dnxz)-0*x5x^3nrh4o5nhzacty3xltpf2sbyj&z9mcpxf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['beautyparlourandshopping-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://beautyparlourandshopping-production.up.railway.app/']
 
 # Application definition
